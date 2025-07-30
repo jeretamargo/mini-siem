72562337f6ea9062f0eb7aa68d50eccf0e29093e
jeretamargo <jeretamargo01@gmail.com>
2025-07-30 19:08:49 -0300

    modulo pg y clase pool biene structuradas y endpoint de prueba d ela base de datos realizado, conexion exitosa

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import {db_pool} from './db.js'
 
 dotenv.config() //Carga las variables de entorno del archivo .env
 
@@ -15,6 +16,19 @@ app.get('/', (req, res)=>{ //primer endpoint de prueba, loguea el metodo http, d
     console.log(`Request  ${req.method} from: ${req.ip}` )
     res.send("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 })
+
+//prueba de la conexion con la bd
+app.get('/bd_test', async (req, res) => {
+    try{
+        const result = await db_pool.query('SELECT NOW()')
+        res.json({success: true, time:result.rows[0]})
+        console.log(`Request  ${req.method} from: ${req.ip}` )
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
 
 app.listen(PORT, () =>{ //loguea el puerto de escucha recien se arranca el server
     console.log(`Backend funcionando y escuchando en: ${PORT}`)
